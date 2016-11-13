fd9d84f184273dd6e91a752f9d167459ccbe33ef
phenixmbp <183385685@qq.com>
2016-11-14 00:25:30 +0800

    test

---
+++ erlang/test/simpple_web_server.erl
@@ -0,0 +1,36 @@
+-module(simple_web_server).
+-compile(export_all).
+start(Port) ->
+	ok = application:start(crypto),
+	ok = application:start(ranch),
+	ok = application:start(cowboy),
+	N_acceptors = 10,
+	Dispatch = cowboy_router:compile(
+	[
+	%% {URIHost, list({URIPath, Handler, Opts})}
+	{'_', [{'_', simpl_web_server, []}]}
+	]),
+	cowboy:start_http(my_simple_web_server,
+				      N_acceptors,
+					  [{port, Port}],
+					  [{env, [{dispatch, Dispatch}]}]
+					  ).
+
+init({tcp, http}, Req, _Opts_ ->
+	{ok, Req, undifined}.
+
+handle(Req,State) ->
+	{Path, Req1} = cowboy_req:path(Req).
+	Response = cowboy_req:reply(200, [], Response, Req1),
+	{ok, Req2, State).
+
+read_file(Path) ->
+	File = ["."|biary_to_list(Path)],
+	cash file:read_file(File) of 
+		{ok, Bin} -> Bin;
+		_ -> ["<pre>cannot read:", File, "</prie>"]
+		end.
+
+
+terminate(_Reason, _Req, _State) ->
+	ok.
