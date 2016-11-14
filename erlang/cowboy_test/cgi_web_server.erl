cfa0a9b7d2c8f8ca081478b7fa8e4307e8917d1c
phenixmbp <183385685@qq.com>
2016-11-15 01:23:44 +0800

    cowboy_test

---
+++ erlang/cowboy_test/cgi_web_server.erl
@@ -0,0 +1,63 @@
+
+-module(cgi_web_server).
+-compile(export_all).
+start(Port) ->
+	ok = application:start(crypto),
+	ok = application:start(ranch),
+	ok = application:start(cowboy),
+	N_acceptors = 10,
+	Dispatch = cowboy_router:compile(
+	[
+	%% {URIHost, list({URIPath, Handler, Opts})}
+	{'_', [{'_', cgi_web_server, []}]}
+	]),
+	cowboy:start_http(http,
+				      N_acceptors,
+					  [{port, Port}],
+					  [{env, [{dispatch, Dispatch}]}]
+					  ).
+
+init({tcp, http}, Req, _Opts_) ->
+	{ok, Req, undifined}.
+
+handle(Req,State) ->
+	{Path, Req1} = cowboy_req:path(Req),
+	handle1(Path, Req1, State).
+
+handle1(<<"/cgi">>, Req, State) ->
+	{Args, Req1} = cowboy_req:qs_vals(Req),
+	{ok, Bin, Req2} = cowboy_req:body(Req1),
+	Val = mochijson2:decode(Bin),
+	Response = call(Args, Val),
+	Json = mochijson2:encode(Response),
+	{ok, Req3} = cowboy_req:reply(200, [], Json, Req2),
+	{ok, Req3, State};
+
+handle1(Path, Req, State) ->
+	Response = read_file(Path),
+	{ok, Req1} = cowboy_req:reply(200, [], Response, Req),
+	{ok, Req1, State}.
+
+read_file(Path) ->
+	File = ["."|binary_to_list(Path)],
+	io:format("11=========>~p~n", [binary_to_list(Path)]),
+	case file:read_file(File) of 
+		{ok, Bin} -> 
+		io:format("22=========>~p~n", [binary_to_list(Bin)]),
+		Bin;
+		_ -> ["<pre>cannot read:", File, "</prie>"]
+	end.
+call([{<<"mod">>, MB}, {<<"func">>, FB}],X) ->
+	Mod = list_to_atom(binary_to_list(MB)),
+	Func = list_to_atom(binary_to_list(FB)),
+	apply(Mod, Func, [X]).
+
+
+
+
+
+
+
+
+terminate(_Reason, _Req, _State) ->
+	ok.
--- erlang/cowboy_test/simple_web_server.erl
+++ erlang/cowboy_test/simple_web_server.erl
@@ -8,7 +8,7 @@ start(Port) ->
 	Dispatch = cowboy_router:compile(
 	[
 	%% {URIHost, list({URIPath, Handler, Opts})}
-	{'_', [{'_', simpl_web_server, []}]}
+	{'_', [{'_', simple_web_server, []}]}
 	]),
 	cowboy:start_http(my_simple_web_server,
 				      N_acceptors,
@@ -16,17 +16,19 @@ start(Port) ->
 					  [{env, [{dispatch, Dispatch}]}]
 					  ).
 
-init({tcp, http}, Req, _Opts_ ->
+init({tcp, http}, Req, _Opts_) ->
 	{ok, Req, undifined}.
 
 handle(Req,State) ->
-	{Path, Req1} = cowboy_req:path(Req).
-	Response = cowboy_req:reply(200, [], Response, Req1),
-	{ok, Req2, State).
+	{Path, Req1} = cowboy_req:path(Req),
+	Response = read_file(Path),
+	io:format("--------->~p~n", [Response]),
+	{ok, Req2} = cowboy_req:reply(200, [], Response, Req1),
+	{ok, Req2, State}.
 
 read_file(Path) ->
-	File = ["."|biary_to_list(Path)],
-	cash file:read_file(File) of 
+	File = ["."|binary_to_list(Path)],
+	case file:read_file(File) of 
 		{ok, Bin} -> Bin;
 		_ -> ["<pre>cannot read:", File, "</prie>"]
 		end.
